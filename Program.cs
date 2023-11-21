87f35fcc4384035a3520e5c1c64168d8043cfd3d
Alexey <alexmail121212@gmail.com>
2023-11-21 19:36:17 +0300

    software v1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,31 @@
-﻿
+﻿string[] arr = { "1", "rrt", "edfgvf", "55", "0" };
+
+Console.Clear();
+
+arrayPrint("Исходный масиив", arr);
+
+arrayPrint("Полученный массив:", takeArrayElementsLessThreeChar(arr));
+
+// Methods
+
+void arrayPrint(string title, string[] array)
+{
+    Console.WriteLine(title);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine($"Элемент {i} = \"{array[i]}\"");
+    }
+    Console.WriteLine();
+}
+
+string[] takeArrayElementsLessThreeChar(string[] array)
+{
+    string[] arrayNew=array;
+    return arrayNew;
+}
+
+
+
 
 
 
